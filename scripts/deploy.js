14648c1dedc12e98bfd2c82e47d5d90aa8f9c09c
Mayela Gonzalez <mayela.gonzalez.mg@gmail.com>
2024-11-27 10:30:03 -0600

    comments deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,21 @@
 const hre = require("hardhat");
 
 async function main() {
-    const [deployer] = await hre.ethers.getSigners();
+    const [deployer] = await hre.ethers.getSigners(); //abstraction to sign messages and transactions
 
     // Replace with your metadata URI and desired owner address
     const baseTokenURI = "https://example.com/metadata/";
-    const initialOwner = deployer.address;
+    const initialOwner = deployer.address; //address of the deployer to provide basic access control (ownable)
 
-    const MyNFT = await hre.ethers.getContractFactory("MyNFT");
-    const myNFT = await MyNFT.deploy(initialOwner, baseTokenURI);
+    const MyNFT = await hre.ethers.getContractFactory("MyNFT"); //send a special type of transaction. an initcode transaction (bytecode)
+    const myNFT = await MyNFT.deploy(initialOwner, baseTokenURI); //method to deploy the smart contract
 
-    await myNFT.deployed();
+    await myNFT.deployed(); //send message after deployment completes
     console.log("MyNFT deployed to:", myNFT.address);
     console.log("Contract ownership assigned to:", initialOwner);
 }
 
-main().catch((error) => {
+main().catch((error) => { //catch error and exit in case anything fails
     console.error(error);
     process.exitCode = 1;
 });
